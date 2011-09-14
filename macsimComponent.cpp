dc17eda93a6e531107cc886840e842aff905c1db
Jaekyu Lee <jaekyu.lee@gatech.edu>
2011-09-14 19:36:16 +0000

    macsim-sst component file
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/trunk@6 76505255-db3a-46bb-8524-017587b0c4bf

---
+++ macsimComponent.cpp
@@ -0,0 +1,119 @@
+#include "sst_config.h"
+
+#include "sst/core/serialization/element.h"
+#include "sst/core/element.h"
+
+#include "macsimComponent.h"
+
+using namespace SST;
+
+macsimComponent::macsimComponent(ComponentId_t id, Params_t& params) : Component(id) {
+	
+	//GET PARAMETERS
+
+	//params.in
+	if ( params.find("paramPath") == params.end() ) {
+		_abort(event_test, "Couldn't find params.in path\n");
+	}
+	paramPath = std::string(params["paramPath"]);
+
+	//trace_file_list
+	if ( params.find("tracePath") == params.end() ) {
+		_abort(event_test, "Couldn't find trace_file_list path\n");
+	}
+	tracePath = std::string(params["tracePath"]);
+
+
+	//statistics out directory
+	if ( params.find("outputPath") == params.end() ) {
+		_abort(event_test, "Couldn't find statistics output directory parameter");
+	}
+	outputPath = std::string(params["outputPath"]);
+
+	//END GET PARAMETERS
+	
+	
+	//Register with clock
+	registerClock("1Ghz",
+	              new Clock::Handler<macsimComponent>(this, 
+                                                    &macsimComponent::ticReceived));
+	
+	//Instantiate Macsim Simulator
+	macsim = new macsim_c();
+	
+}
+
+macsimComponent::macsimComponent() : Component(-1) {} //for serialization only
+
+
+int macsimComponent::Setup() {
+	printf("Initializing macsim simulation state\n");
+	
+	//Format for initialization protocol
+	char* argv[3];
+	argv[0] = new char[ paramPath.size()+1]; strcpy(argv[0],  paramPath.c_str());
+	argv[1] = new char[ tracePath.size()+1]; strcpy(argv[1],  tracePath.c_str());
+	argv[2] = new char[outputPath.size()+1]; strcpy(argv[2], outputPath.c_str());
+		
+	//Pass paramaters to simulator if applicable
+	macsim->initialize(0, argv);
+
+	//cleanup
+	delete argv[0]; delete argv[1]; delete argv[2];
+
+	return 0;
+}
+
+int macsimComponent::Finish() {
+	printf("Simulation Finished, Finalizing");
+
+  macsim->finalize();
+
+	return 0;
+}
+
+/*******************************************************
+ *  ticReceived
+ *    return value
+ *      true  : indicates the component finished; 
+ *              no more clock events to the component
+ *      false : component not done yet
+ *******************************************************/
+bool macsimComponent::ticReceived( Cycle_t ) {
+	
+	//Run a cycle of the simulator
+	bool simRunning = macsim->run_a_cycle();
+
+	//true indicates -> end of life for component 
+	return (simRunning==false);
+
+}
+
+// Element Libarary / Serialization stuff
+
+BOOST_CLASS_EXPORT(macsimComponent)
+
+static Component* create_macsimComponent(SST::ComponentId_t id,
+                                         SST::Component::Params_t& params)
+{
+	return new macsimComponent( id, params );
+}
+
+static const ElementInfoComponent components[] = {
+	{ 
+		"macsimComponent",
+		"MACSIM Simulator",
+		NULL,
+		create_macsimComponent
+	},
+	{ NULL, NULL, NULL, NULL }
+};
+
+extern "C" {
+	ElementLibraryInfo macsimComponent_eli = 
+	{
+		"macsimComponent",
+		"MACSIM Simulator",
+		components,
+	};
+}
---
+++ macsimComponent.h
@@ -0,0 +1,47 @@
+#ifndef _MACSIM_COMPONENT_H
+#define _MACSIM_COMPONENT_H
+
+#include <sst/core/event.h>
+#include <sst/core/sst_types.h>
+#include <sst/core/component.h>
+#include <sst/core/timeConverter.h>
+
+#include "src/macsim.h"
+
+class macsimComponent : public SST::Component {
+	
+	public:
+		macsimComponent(SST::ComponentId_t id, SST::Component::Params_t& params);
+		int Setup();
+		int Finish();
+		
+	private:
+		macsimComponent();   // for serialization only
+		macsimComponent(const macsimComponent&);   // do not implement
+		void operator=(const macsimComponent&); // do not implement
+	
+		virtual bool ticReceived(SST::Cycle_t);
+		
+		//MEMBER VARIABLES
+		std::string  paramPath;
+		std::string  tracePath;
+		std::string outputPath;
+		macsim_c* macsim;
+		
+		//FOR SERIALIZATION
+		friend class boost::serialization::access;
+		template<class Archive>
+		void save(Archive & ar, const unsigned int version) const 
+		{
+			ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Component);
+		}
+		
+		template<class Archive>
+		void load(Archive & ar, const unsigned int version) 
+		{
+			ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Component);
+		}
+		
+		BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+#endif /* _MACSIM_COMPONENT_H */
