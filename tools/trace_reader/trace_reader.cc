5c23a70066e2f4b9bb521347fa651e6b941c3143
Jaekyu Lee <jaekyu.lee@gatech.edu>
2011-09-27 18:22:59 +0000

    files added
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/trunk@25 76505255-db3a-46bb-8524-017587b0c4bf

---
+++ tools/trace_reader/trace_reader.cc
@@ -0,0 +1,131 @@
+#include "trace_reader.h"
+#include <iostream>
+#include "all_knobs.h"
+
+trace_reader_c trace_reader_c::Singleton;
+
+
+trace_reader_c::trace_reader_c()
+{
+}
+
+void trace_reader_c::init()
+{
+  if (g_knobs->KNOB_ENABLE_REUSE_DIST->getValue()) {
+    trace_reader_c* reuse_distance = new reuse_distance_c;
+    m_tracer.push_back(reuse_distance);
+  }
+
+  if (g_knobs->KNOB_ENABLE_COUNT_STATIC_PC->getValue()) {
+    trace_reader_c* static_pc = new static_pc_c;
+    m_tracer.push_back(static_pc);
+  }
+}
+
+void trace_reader_c::reset()
+{
+  for (int ii = 0; ii < m_tracer.size(); ++ii) 
+    m_tracer[ii]->reset();
+}
+
+
+trace_reader_c::~trace_reader_c()
+{
+  for (int ii = 0; ii < m_tracer.size(); ++ii) {
+    delete m_tracer[ii];
+  }
+
+  m_tracer.clear();
+}
+
+void trace_reader_c::inst_event(trace_info_s* inst)
+{
+  for (int ii = 0; ii < m_tracer.size(); ++ii) {
+    m_tracer[ii]->inst_event(inst);
+  }
+}
+
+
+void trace_reader_c::print()
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+reuse_distance_c::reuse_distance_c()
+{
+  m_name = "reuse_distance";
+  m_self_counter = 0;
+}
+
+
+reuse_distance_c::~reuse_distance_c()
+{
+}
+
+
+void reuse_distance_c::inst_event(trace_info_s* inst)
+{
+  return ;
+  if (static_cast<int>(inst->m_num_ld) > 0 or inst->m_has_st == true) {
+    ++m_self_counter;
+    Addr addr;
+    if (inst->m_has_st) {
+      addr = inst->m_st_vaddr;
+    } else {
+      addr = inst->m_ld_vaddr1;
+    }
+
+    addr = addr >> 6;
+
+    if (m_reuse_map.find(addr) != m_reuse_map.end()) {
+      bool same_pc = false;
+      if (inst->m_instruction_addr == m_reuse_pc_map[addr]) same_pc = true;
+      cout << dec << m_self_counter << " dist:" << dec << m_self_counter - m_reuse_map[addr] << " addr:" 
+           << hex << addr << " pc:" << hex << m_reuse_pc_map[addr] << " " << hex << inst->m_instruction_addr << "\n";
+    }
+    else {
+      cout << hex << inst->m_instruction_addr << "\n";
+    }
+    m_reuse_map[addr] = m_self_counter;
+    m_reuse_pc_map[addr] = inst->m_instruction_addr;
+  }
+}
+
+
+void reuse_distance_c::print()
+{
+  std::cout << m_name << "\n";
+}
+
+
+void reuse_distance_c::reset()
+{
+  m_reuse_map.clear();
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+static_pc_c::static_pc_c() {}
+
+static_pc_c::~static_pc_c() 
+{
+  cout << "Total static pc:" << m_static_pc.size() << "\n";
+  cout << "Total static memory pc:" << m_static_mem_pc.size() << "\n";
+
+  m_static_pc.clear();
+  m_static_mem_pc.clear();
+}
+
+void static_pc_c::inst_event(trace_info_s* inst)
+{
+  m_static_pc[inst->m_instruction_addr] = true;
+  if (static_cast<int>(inst->m_num_ld) > 0 or inst->m_has_st == true) {
+    m_static_mem_pc[inst->m_instruction_addr] = true;
+  }
+}
+
---
+++ tools/trace_reader/trace_reader.h
@@ -0,0 +1,62 @@
+#ifndef TRACE_READER_H
+#define TRACE_READER_H
+
+#include "trace_read.h"
+#include "global_types.h"
+#include <vector>
+#include <unordered_map>
+
+
+extern all_knobs_c* g_knobs;
+
+
+class trace_reader_c
+{
+  public:
+    trace_reader_c();
+    virtual ~trace_reader_c();
+    virtual void inst_event(trace_info_s* inst);
+    virtual void print();
+    virtual void reset();
+
+    void init();
+    static trace_reader_c Singleton;
+
+  protected:
+    std::vector<trace_reader_c*> m_tracer;
+    std::string m_name;
+};
+
+
+class reuse_distance_c : public trace_reader_c
+{
+  public:
+    reuse_distance_c();
+    ~reuse_distance_c();
+    void inst_event(trace_info_s* inst);
+    void print();
+    void reset();
+
+  private:
+    int m_self_counter;
+    std::unordered_map<Addr, int> m_reuse_map;
+    std::unordered_map<Addr, int> m_reuse_pc_map;
+    std::map<Addr, bool> m_static_pc;
+    std::unordered_map<Addr, std::unordered_map<Addr, std::unordered_map<int, bool> *> *> m_result;
+};
+
+
+class static_pc_c : public trace_reader_c
+{
+  public:
+    static_pc_c();
+    ~static_pc_c();
+    void inst_event(trace_info_s* inst);
+
+  private:
+    std::unordered_map<Addr, bool> m_static_pc;
+    std::unordered_map<Addr, bool> m_static_mem_pc;
+};
+
+
+#endif
