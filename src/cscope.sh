02b36991fc818d20d7c8116865c480bb8f8d6f1f
Jaekyu Lee <jaekyu.lee@gatech.edu>
2011-11-30 21:47:40 +0000

    [misc]
    1. cscope script added
    
    
    
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/trunk@145 76505255-db3a-46bb-8524-017587b0c4bf

---
+++ src/cscope.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -f 'cscope.files' ] 
+then
+  rm -f 'cscope.files'
+fi
+
+find -name '*.cc'   > 'cscope.files'
+find -name '*.cpp'  > 'cscope.files'
+find -name '*.h'   >> 'cscope.files'
+cscope -b
