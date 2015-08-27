0607427ed9c6e2b104d038c1bc9c8e7f8dcf8bf4
Hyesoon Kim <hyesoon@cc.gatech.edu>
2015-08-27 19:40:47 +0000

    memory trace generations
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/branch/hmc2@608 76505255-db3a-46bb-8524-017587b0c4bf

--- tools/mem_trace_generator/mem_trace.cc
+++ tools/mem_trace_generator/mem_trace.cc
@@ -17,6 +17,9 @@ typedef unsigned __int32 uint32_t;
 #include <fstream>
 #include <sstream>
 #include <cstring>
+#include <cassert> 
+#include <string> 
+#include <ctype.h> 
 
 #include "mem_trace.h"
 
@@ -108,9 +111,35 @@ void set_mem_op(trace_info_cpu_s *mem_op, uint64_t inst_addr, uint64_t addr1, bo
 
 int main(int argc, char *argv[])
 {
+  int threadnum = 1; 
+  /*
+  ifstream fname='trace1.txt'; 
+  trace_generation_mode = 0; 
+  if (argc < 2) 
+    printf("Usage: %s <dram-read-file>. Defalt is generating stream memory addresses \n"); 
+  if (argc >= 3) {
+    fname = 
+    fname =(argv[1]); 
+    fname << argv[1]; 
+    trace_generation_mode = 1; 
+  }
+  if (argc > = 4)  { 
+    fname = argv[1];
+    fname << argv[1]; 
+    threadnum = strtol(argv[2], NULL, 10); 
+    trace_generation_mode = 1; 
+  }
+  */
   printf("generating traces \n");
 
-  for (int threadid = 0; threadid < 1; threadid++) {
+  // read arguments 
+  // number of threads 
+  // input address trace file name 
+  // output trace file name 
+  // trace generation mode: 0 (default) sequential stream 1: read from a file 
+  int trace_generation_mode = 1; 
+
+  for (int threadid = 0; threadid < threadnum; threadid++) {
 
     stringstream sstream;
     sstream << "coh_trace.1" << "_" << threadid << ".raw";
@@ -130,31 +159,76 @@ int main(int argc, char *argv[])
     init_mem_op(mem_trace);
     init_fence_op(fence_trace);
 
-    for (int ii = 0; ii < 1000; ii++) {
-      uint64_t t_addr;
-      static uint64_t inst_addr;
+    //   char fname[256] = "trace1.txt"; 
+    char fname[256] = "hmc_dram.trace"; 
 
-      if (inst_addr ==1024) inst_addr = 0;
-
+    switch(trace_generation_mode) { 
+    case 0: 
+      /* default stream trace generation part */ 
+      for (int ii = 0; ii < 1000; ii++) {
+	uint64_t t_addr;
+	static uint64_t inst_addr;
+	
+	if (inst_addr ==1024) inst_addr = 0;
+	
 #define INDEP_TRACE
 #ifdef INDEP_TRACE
-      t_addr = ii *64  + threadid*1024*1024;
+	t_addr = ii *64  + threadid*1024*1024;
 #endif
 #ifdef DEP_TRACE
-      t_addr = ii*64;
+	t_addr = ii*64;
 #endif
-
-      set_mem_op(mem_trace, (++inst_addr)*2, t_addr, 0);  // 0: read operation 1: write operation //
-      gzwrite(trace_output, mem_trace,  sizeof(trace_info_cpu_s));
-
-      if (((inst_addr%4) != 0) && (t_addr > 1024))  {
-        set_mem_op(mem_trace, (++inst_addr)*2+1, t_addr-1024,  1);
-        gzwrite(trace_output, mem_trace,  sizeof(trace_info_cpu_s));
+	
+	set_mem_op(mem_trace, (++inst_addr)*2, t_addr, 0);  // 0: read operation 1: write operation //
+	gzwrite(trace_output, mem_trace,  sizeof(trace_info_cpu_s));
+	
+	if (((inst_addr%4) != 0) && (t_addr > 1024))  {
+	  set_mem_op(mem_trace, (++inst_addr)*2+1, t_addr-1024,  1);
+	  gzwrite(trace_output, mem_trace,  sizeof(trace_info_cpu_s));
+	}
+	
+	if (((inst_addr%8) != 0))  {
+	  gzwrite(trace_output, fence_trace,  sizeof(trace_info_cpu_s)); 
+	}
       }
 
-      if (((inst_addr%8) != 0))  {
-        gzwrite(trace_output, fence_trace,  sizeof(trace_info_cpu_s)); 
+    case 1: {
+      // file open 
+      // while 
+      ifstream myfile (fname);
+      string line;
+      uint64_t t_addr;  
+      size_t pos; 
+      int req_type = 0; 
+      static uint64_t inst_addr;
+      
+      if (myfile.is_open()) { 
+	while (getline(myfile, line)) {
+	  // cout << line << '\n'; 
+	  // t_addr = strtol(line.c_str(), &pos, 16); 
+	  t_addr = std::stoul(line, &pos, 16); 
+	  pos = line.find_first_not_of(' ', pos+1); 
+	  
+	  if (pos == string::npos || line.substr(pos)[0] == 'R') 
+	    req_type = 0; // read 
+	  else if (line.substr(pos)[0] == 'W') 
+	    req_type = 1; // write 
+	  cout << "addr:" << hex << t_addr; 
+	  if (req_type == 1 ) cout << " R" << endl; 
+	  else cout << " W" << endl; 
+
+	  if (inst_addr > 1024) inst_addr = 0; 
+	  if (req_type  == 0 ) set_mem_op(mem_trace, (++inst_addr)*2, t_addr, req_type);  // 0: read operation 1: write operation //
+	  else set_mem_op(mem_trace, (++inst_addr)*2+1, t_addr, req_type);  // 0: read operation 1: write operation //
+	  gzwrite(trace_output, mem_trace,  sizeof(trace_info_cpu_s));
+
+	}
+	myfile.close();
       }
+      else 
+	cout << " Unable to open file\n"; 
+    }
+
     }
 
     gzclose(trace_output);
