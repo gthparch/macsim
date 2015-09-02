2965208edd215a5ba9867e0720613f24db14cd23
Ramya Hadidi <rhadidi@gatech.edu>
2015-09-02 21:20:27 +0000

    Changes to memEvent, added types to HMC type enum
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/branch/hmc2@627 76505255-db3a-46bb-8524-017587b0c4bf

--- src/uop.h
+++ src/uop.h
@@ -485,7 +485,16 @@ typedef enum HMC_Type_enum
     HMC_CAS_zero_16B=2,
     HMC_CAS_greater_16B=3,
     HMC_CAS_less_16B=4,
-    HMC_ADD_16B=5
+    HMC_ADD_16B=5,
+    HMC_ADD_8B=6,
+    HMC_ADD_DUAL=7,
+    HMC_SWAP=8,
+    HMC_BIT_WR=9,
+    HMC_AND=10,
+    HMC_NAND=11,
+    HMC_OR=12,
+    HMC_XOR=13,
+    NUM_HMC_TYPES=14
 } HMC_Type;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
