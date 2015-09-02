5d9e53dda9738f6d22459712ab11e9b506d61f58
Ramya Hadidi <rhadidi@gatech.edu>
2015-09-02 21:33:35 +0000

    seperating enum for HMC_type
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/branch/hmc2@628 76505255-db3a-46bb-8524-017587b0c4bf

--- src/uop.h
+++ src/uop.h
@@ -44,6 +44,8 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "global_types.h"
 #include "global_defs.h"
 
+#include "hmc_types.h"
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /// \brief allocation queue types
@@ -477,25 +479,6 @@ typedef enum Dep_Type_enum {
   PREV_UOP_DEP,
   NUM_DEP_TYPES,
 } Dep_Type;
-
-typedef enum HMC_Type_enum
-{
-    HMC_NONE=0,
-    HMC_CAS_equal_16B=1,
-    HMC_CAS_zero_16B=2,
-    HMC_CAS_greater_16B=3,
-    HMC_CAS_less_16B=4,
-    HMC_ADD_16B=5,
-    HMC_ADD_8B=6,
-    HMC_ADD_DUAL=7,
-    HMC_SWAP=8,
-    HMC_BIT_WR=9,
-    HMC_AND=10,
-    HMC_NAND=11,
-    HMC_OR=12,
-    HMC_XOR=13,
-    NUM_HMC_TYPES=14
-} HMC_Type;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /// \brief Source uop information class
