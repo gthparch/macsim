67e776af6540aeb8c13aa2ff9613eb7c20c8e44c
Ramyad Hadidi <rhadidi@gatech.edu>
2015-11-17 17:31:57 -0500

    fix new hmc_type erros

--- src/hmc_types.h
+++ src/hmc_types.h
@@ -57,8 +57,9 @@ class hmc_type_c
             case HMC_COMP_less:     return std::string("HMC_COMP_less");
             case HMC_COMP_equal:    return std::string("HMC_COMP_equal");
             case HMC_CANDIDATE:     return std::string("HMC_CANDIDATE");
-            case HMC_hook:          return std::string("std::__detail::_List_node_base::_M_hook");
-            case HMC_unhook:        return std::string("std::__detail::_List_node_base::_M_unhook");
+            case HMC_TRANS_BEG:     return std::string("HMC_TRANS_BEG");
+            case HMC_TRANS_MID:     return std::string("HMC_TRANS_MID");
+            case HMC_TRANS_END:     return std::string("HMC_TRANS_END");
             case NUM_HMC_TYPES:
             default: return std::string("");
         }
@@ -83,8 +84,9 @@ class hmc_type_c
         else if (name=="HMC_COMP_less")    return HMC_COMP_less;
         else if (name=="HMC_COMP_equal")   return HMC_COMP_equal;
         else if (name=="HMC_CANDIDATE")    return HMC_CANDIDATE;
-        else if (name=="std::__detail::_List_node_base::_M_hook")          return HMC_hook;
-        else if (name=="std::__detail::_List_node_base::_M_unhook")        return HMC_unhook;
+        else if (name=="HMC_TRANS_BEG")    return HMC_TRANS_BEG;
+        else if (name=="HMC_TRANS_MID")    return HMC_TRANS_MID;
+        else if (name=="HMC_TRANS_END")    return HMC_TRANS_END;
         else return HMC_NONE;
     }
 
