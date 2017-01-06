e15191936213a16b2f43eb68a9cf6944a75c5ba3
Hyojong Kim <hyojong.kim@gatech.edu>
2017-01-06 18:49:36 +0900

    adding igpu (short for intel gpu) core type

--- src/trace_read.h
+++ src/trace_read.h
@@ -131,6 +131,32 @@ typedef struct trace_info_a64_s {
   uint64_t m_instruction_next_addr; /**< next pc address, not in raw trace format */
 } trace_info_a64_s;
 
+typedef struct trace_info_igpu_s {
+  uint8_t  m_num_read_regs;     /**< num read registers */
+  uint8_t  m_num_dest_regs;     /**< num dest registers */
+  uint8_t  m_src[MAX_SRC_NUM];  /**< src register id */
+  uint8_t  m_dst[MAX_DST_NUM];  /**< dest register id */
+  uint8_t  m_cf_type;           /**< branch type */
+  bool     m_has_immediate;     /**< has immediate field */
+  uint8_t  m_opcode;            /**< opcode */
+  bool     m_has_st;            /**< has store operation */
+  bool     m_is_fp;             /**< fp operation */
+  bool     m_write_flg;         /**< write flag */
+  uint8_t  m_num_ld;            /**< number of load operations */
+  uint8_t  m_size;              /**< instruction size */
+  // dynamic information
+  uint64_t m_ld_vaddr1;         /**< load address 1 */
+  uint64_t m_ld_vaddr2;         /**< load address 2 */
+  uint64_t m_st_vaddr;          /**< store address */
+  uint64_t m_instruction_addr;  /**< pc address */
+  uint64_t m_branch_target;     /**< branch target address */
+  uint8_t  m_mem_read_size;     /**< memory read size */
+  uint8_t  m_mem_write_size;    /**< memory write size */
+  bool     m_rep_dir;           /**< repetition direction */
+  bool     m_actually_taken;    /**< branch actually taken */
+  uint64_t m_instruction_next_addr; /**< next pc address, not in raw trace format */
+} trace_info_igpu_s;
+
 // identical to structure in trace generator
 typedef struct trace_info_gpu_small_s {
   uint8_t m_opcode;
