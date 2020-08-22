0dd684feff1213296f44b8df83deed36168c8a89
hyojongk <hyojong.kim@gatech.edu>
2020-08-22 15:06:41 -0400

    apply patch to macsim_event

--- macsimEvent.h
+++ macsimEvent.h
@@ -1,23 +1,31 @@
 #ifndef MACSIM_EVENT_H
 #define MACSIM_EVENT_H
 
-namespace SST { namespace MacSim {
+namespace SST {
+namespace MacSim {
 
 enum MacSimEventType { NONE, START, FINISHED };
 
-class MacSimEvent : public SST::Event {
-	public:
-    MacSimEvent(MacSimEventType type = NONE) : SST::Event(), m_type(type) {}
-    ~MacSimEvent() {}
+class MacSimEvent : public SST::Event
+{
+public:
+  MacSimEvent(MacSimEventType type = NONE) : SST::Event(), m_type(type) {
+  }
+  ~MacSimEvent() {
+  }
 
-    MacSimEventType getType() { return m_type; }
-    void setType(MacSimEventType type) { m_type = type; }
+  MacSimEventType getType() {
+    return m_type;
+  }
+  void setType(MacSimEventType type) {
+    m_type = type;
+  }
 
-    NotSerializable(MacSimEvent)
+  NotSerializable(MacSimEvent)
 
-  private:
-    MacSimEventType m_type;
+    private : MacSimEventType m_type;
 
-}; // class MacSimEvent
-}}
-#endif //MACSIM_EVENT_H
+};  // class MacSimEvent
+}
+}
+#endif  //MACSIM_EVENT_H
