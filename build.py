30a376df8980f81beb22176b4e17c3400d95fe4f
Pranith Kumar <bobby.prani@gmail.com>
2021-01-23 20:49:55 -0800

    Update scons to python3

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 #########################################################################################
 # Author      : Jaekyu Lee (jq.lee17@gmail.com)
