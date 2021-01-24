86db18a06c5fbfabbacda362dbd724344c3b6a33
Pranith Kumar <bobby.prani@gmail.com>
2021-01-24 14:39:59 -0800

    Update build.py to use python3

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 #########################################################################################
 # Author      : Jaekyu Lee (jq.lee17@gmail.com)
